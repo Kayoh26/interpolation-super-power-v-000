c35d241ff63385e74e08fddac98a85fc349c966f
Karen O <k.ortiz026@gmail.com>
2018-03-06 00:19:13 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,7 +1,5 @@
 # Write your #display_rainbow method here
 def display_rainbow (color_array)
-  for each |i| do
-    
-  end
+  puts "R: #{color_array[0]}, O: #{color_array[1]}, Y:  #{color_array[2]}, G: #{color_array[3]}, B: #{color_array[4]}, I: #{color_array[5]}, v: #{color_array[6]}"
 
 end
